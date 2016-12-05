57551be93e7de52ece42affbe6c77b6adb9a5faa
Conor Smith <conorjbsmith@gmail.com>
2016-12-05 09:44:18 +0000

    Remove obsolete code

--- app/Holdable.php
+++ app/Holdable.php
@@ -5,13 +5,6 @@ namespace ConorSmith\Tbtag;
 
 interface Holdable
 {
-    const EMP = "EMP";
-    const GRAVY = "Gravy";
-    const PHONE = "Phone";
-    const RIFLE = "Rifle";
-    const SANDWICH = "Sandwich";
-    const SUNGLASSES = "Sunglasses";
-
     public function getPickUpEvents(): array;
     public function getDropEvents(): array;
     public function getSlug(): string;
